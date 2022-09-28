3e9818579a40876af01906b3c19f82b7df37bc0f
lvojp <920@wisdomtrees.net>
2022-09-29 00:16:41 +0900

    complete size_convertor

--- src/size_convertor.py
+++ src/size_convertor.py
@@ -1,11 +1,10 @@
 import cv2
 import os
+import glob
+import argparse
 
 
 class SizeConvertor:
-    def __init__(self, path: str) -> None:
-        self.cv2_img = cv2.imread(path)
-
     def is_horizontal(self, img: cv2) -> bool:
         return img.shape[0] < img.shape[1]
 
@@ -17,14 +16,46 @@ class SizeConvertor:
         result = cv2.resize(img, dsize=(width, height))
         return result
 
-    def main(self) -> None:
-        img = self.cv2_img
-        if self.is_horizontal(img):
-            result = self.scale_to_width(img, width=1440)
-        else:
-            result = self.scale_to_width(img, width=1220)
+    def make_dest_path(self, img_path: str, cv2_img: cv2) -> str:
+        org_path_dir = os.path.dirname(img_path)
+        org_path_name = os.path.basename(img_path)
+        width = cv2_img.shape[1]
+        dest_dir = os.path.join(org_path_dir, f"{width}x")
+        os.makedirs(dest_dir, exist_ok=True)
+        result = os.path.join(dest_dir, org_path_name)
         return result
+
+    def main(self, img_path: str, horizontal_width: int = 1440, portrait_width: int = 1220) -> None:
+        cv2_img = cv2.imread(img_path)
+        if self.is_horizontal(cv2_img):
+            resized_cv2_img = self.scale_to_width(cv2_img, width=horizontal_width)
+        else:
+            resized_cv2_img = self.scale_to_width(cv2_img, width=portrait_width)
+        dest_path = self.make_dest_path(img_path, resized_cv2_img)
+        cv2.imwrite(dest_path, resized_cv2_img)
 
 
 if __name__ == '__main__':
-    print('test')
+    parser = argparse.ArgumentParser(description='auto resize images, each horizontal image and portrait image.')
+    parser.add_argument('-path', '--img_path_dir', required=True, type=str, help='image path directory')
+    parser.add_argument('-hw', '--horizontal_width', type=int, help='horizontal image width')
+    parser.add_argument('-pw', '--portrait_width', type=int, help='portrait image width')
+    args = parser.parse_args()
+    dir_path = args.img_path_dir
+    hw = args.horizontal_width
+    pw = args.portrait_width
+
+    sc = SizeConvertor()
+    available_extends = ["jpg", "jpeg", "png"]
+    for ext in available_extends:
+        for img_path in glob.glob(f"{dir_path}/*{ext}"):
+            print('resize:', img_path)
+            if hw is not None and pw is not None:
+                sc.main(img_path, horizontal_width=hw, portrait_width=pw)
+            elif hw is not None:
+                sc.main(img_path, horizontal_width=hw)
+            elif pw is not None:
+                sc.main(img_path, portrait_width=pw)
+            else:
+                sc.main(img_path)
+            print('finish:', img_path)
--- tests/test_size_convertor.py
+++ tests/test_size_convertor.py
@@ -1,4 +1,5 @@
 import cv2
+import shutil
 from src.size_convertor import SizeConvertor
 
 
@@ -11,14 +12,34 @@ def test_is_horizontal():
     assert sc.is_horizontal(img) is True
 
 
-def test_main():
+def test_scale_to_width():
     sc = SizeConvertor()
-    # portrait
     img_in = cv2.imread("./assets/size_convertor/input/portrait.jpg")
-    img_out = sc.main(img_in)
-    cv2.imwrite("./assets/size_convertor/output/portrait.jpg", img_out)
+    img_out = sc.scale_to_width(img_in, 1220)
+    assert img_out.shape[1] == 1220
 
-    # horizontal
     img_in = cv2.imread("./assets/size_convertor/input/horizontal.jpg")
-    img_out = sc.main(img_in)
-    cv2.imwrite("./assets/size_convertor/output/horizontal.jpg", img_out)
+    img_out = sc.scale_to_width(img_in, 1440)
+    assert img_out.shape[1] == 1440
+
+
+def test_make_dest_path():
+    sc = SizeConvertor()
+    img_in_path = "./assets/size_convertor/input/portrait.jpg"
+    img_in_cv2 = cv2.imread(img_in_path)
+    result = sc.make_dest_path(img_in_path, img_in_cv2)
+    print(result)
+    assert result == "./assets/size_convertor/input/4000x/portrait.jpg"
+
+
+def test_main(tmpdir):
+    shutil.copy("./assets/size_convertor/input/portrait.jpg", tmpdir)
+    shutil.copy("./assets/size_convertor/input/horizontal.jpg", tmpdir)
+
+    sc = SizeConvertor()
+    sc.main(f"{tmpdir}/portrait.jpg")
+    sc.main(f"{tmpdir}/horizontal.jpg")
+
+    assert cv2.imread(f"{tmpdir}/1220x/portrait.jpg") is not None
+    assert cv2.imread(f"{tmpdir}/1440x/horizontal.jpg") is not None
+
